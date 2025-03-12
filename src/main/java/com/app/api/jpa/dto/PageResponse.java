6658d91efa305f20a19ac81c843d63a45e31dafe
gyj <gyj@gmail.com>
2025-03-13 00:10:02 +0900

    Search 목록 조회를 위한 공통 클래스

---
+++ src/main/java/com/app/api/jpa/dto/PageResponse.java
@@ -0,0 +1,76 @@
+package com.app.api.jpa.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PageResponse<T> {
+
+  List<T> list;
+  long totalCount;
+  int page;
+  int limit;
+  int pageSize;
+
+  @Getter
+  @Setter
+  public static class PageResponseData {
+    private long listNumber;
+  }
+
+  public List<T> getList() {
+    if (list == null) {
+      return List.of();
+    }
+
+    if (list.isEmpty() || !(list.get(0) instanceof PageResponseData)) {
+      return list;
+    }
+
+    for (int i = 0; i < list.size(); i++) {
+      ((PageResponseData) list.get(i)).setListNumber(totalCount - (long) (page - 1) * limit - i);
+    }
+
+    return list;
+  }
+
+  public int getTotalPage() {
+    return (int) Math.ceil((double) totalCount / limit);
+  }
+
+  public int getStartPage() {
+    return ((page - 1) / pageSize) * pageSize + 1;
+  }
+
+  public int getEndPage() {
+    return Math.min(getStartPage() + pageSize - 1, getTotalPage());
+  }
+
+  @JsonProperty("hasPrev")
+  public boolean hasPrev() {
+    return getStartPage() > 1;
+  }
+
+  @JsonProperty("hasNext")
+  public boolean hasNext() {
+    return getEndPage() < getTotalPage();
+  }
+
+  @JsonProperty("isFirst")
+  public boolean isFirst() {
+    return getStartPage() == 1;
+  }
+
+  @JsonProperty("isLast")
+  public boolean isLast() {
+    return getEndPage() == getTotalPage();
+  }
+}
---
+++ src/main/java/com/app/api/jpa/dto/Search.java
@@ -0,0 +1,42 @@
+package com.app.api.jpa.dto;
+
+import com.app.api.utils.StringUtils;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+@Getter
+@SuperBuilder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Search {
+
+    @Builder.Default
+    int page = 1;
+
+    @Builder.Default
+    int limit = 10;
+
+    @Builder.Default
+    int pageSize = 10;
+
+    @Builder.Default
+    String order = "createdOn";
+
+    @Builder.Default
+    String direction = "desc";
+
+    String search;
+    String keyword;
+
+    public PageRequest toPageRequest() {
+        return PageRequest.of(page - 1, limit, Sort.Direction.valueOf(direction.toUpperCase()), order);
+    }
+
+    public boolean hasSearch() {
+        return StringUtils.hasAllText(search, keyword);
+    }
+}
